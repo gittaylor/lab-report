94677f1c679f59ac71e9a9a076f5d8cd5808e2f8
ts <ts@ts.local>
2013-03-13 19:43:05 -0700

    proofing changes; fixing damaged repository

--- heme_reports.py
+++ heme_reports.py
@@ -39,17 +39,30 @@ def get_hexa_results(filename, outdir):
 
 if __name__ == '__main__':
     from sys import argv
-    if argv[1] == 'rvvt_pts':
+    if argv[1] == 'rvvt_inrs':
         hexa_txt, rvvt_txt = read_hexa_rvvt_worksheet(argv[2])
         rvvt = ht.RVVT()
         rvvt.parse_text(rvvt_txt)
-        for mrn in rvvt.requires_pt():
+        for mrn in rvvt.requires_inr():
             print(mrn + '\r')
+    elif argv[1] == 'report_templates':
+        hexa_txt, rvvt_txt = read_hexa_rvvt_worksheet(argv[2])
+        rvvt = ht.RVVT()
+        rvvt.parse_text(rvvt_txt)
+        hexa = ht.HEXA()
+        hexa.parse_text(hexa_txt)
+        hexa.make_reports(open(argv[4]).read(), 'HEXA', argv[5])
+        rvvt.make_reports(open(argv[3]).read(), 'RVVT', argv[5])
 
     elif argv[1] == 'interpret_rvvts_hexas':
         hexa_txt, rvvt_txt = read_hexa_rvvt_worksheet(argv[2])
-        pt_txt = read_pts(argv[3])
-        pt = ht.PT()
-        pt.parse_text(pt_txt)
-        latest_pts = pt.get_latest()
+        rvvt = ht.RVVT()
+        rvvt.parse_text(rvvt_txt)
+        inr = ht.PT()
+        inr.parse_text(open(argv[3]).read())
+        hexa = ht.HEXA()
+        hexa.parse_text(hexa_txt)
+        rvvt.make_reports(hexa, inr, argv[4])
+        hexa.make_reports(rvvt, inr, argv[4])
+
         
--- heme_tests.py
+++ heme_tests.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 import re
 from datetime import datetime
+from os import path
 
 class Result(float):
     def __new__(self, val, flag=None, other={}):
@@ -11,13 +12,20 @@ class Result(float):
 class RVVTresult(Result):
     def __new__(self, result):
         res = result.split(',')
+        if len(res) == 1:
+            return super(RVVTresult, self).__new__(self, res[0])
         if len(res) == 2:
-            return super(Result, self).__new__(self, res[0], other={'st': res[1]})
+            return super(RVVTresult, self).__new__(self, res[0], other={'st': res[1]})
         elif len(res) == 3:
-            return super(Result, self).__new__(self, res[0], flag=res[2])
+            return super(RVVTresult, self).__new__(self, res[0], flag=res[2])
         else:
             raise ValueError("cannot parse result: %s" % result)
 
+class HEXAresult(str):
+    def __new__(self, val, flag=None, other={}):
+        self.flag = flag
+        self.other = other
+        return super(HEXAresult, self).__new__(self, val)
 
 class Test(object):
     re_date = "(\d\d\/\d\d\/\d\d\d\d)"
@@ -28,31 +36,38 @@ class Test(object):
     re_wo_info1 = "\s+".join([re_acc, re_date, re_time, re_name])
     re_wo_info2 = "\s+%s\s+\w+\s+%s" % (re_mrn, re_date)
 
-    def make_report(template, **vars):
-        f = open(self.__getattribute__(template))
-        text = f.read()
-        f.close()
-        return self.replace(text, vars)
+    # TODO:  consolidate the make_reports functions to this
+    # def make_report(template, **vars):
+    #     f = open(self.__getattribute__(template))
+    #     text = f.read()
+    #     f.close()
+    #     return self.replace(text, vars)
 
     def parse_result(self, result):
         return Result(result)
 
     def parse_text(self, text):
         results = {}
+        # info1 = [i for i in re.finditer(self.re_wo_info1, text)]
+        # info2 = [i for i in re.finditer(self.re_wo_info2, text)]
+        # result = [i for i in re.finditer(self.re_result, text)]
+        # import pdb; pdb.set_trace()
         for i, j, k in zip(re.finditer(self.re_wo_info1, text), re.finditer(self.re_wo_info2, text), re.finditer(self.re_result, text)):
             # sanity check to make sure the regex results are near each other
             # if wo_info2.start() - wo_info1.start() < 0 or > 200 these are likely from different entries
             # if re_result.start() - wo_info2.start() < 0 or > 400 these are likely from different entries
-
+            
             if j.start() - i.start() < 0 or j.start() - i.start() > 200:
                 raise ValueError("error parsing result near position %s" % min(i.start(), j.start()))
             if k.start() - j.start() < 0 or k.start() - j.start() > 400:
                 raise ValueError("error parsing result near position %s" % min(j.start(), k.start()))
             
             acc, accdate, acctime, name = i.groups()
+            if len(acctime) == 4:
+                acctime = acctime[:2] + ':' + acctime[2:]
             mrn, bdate = j.groups()
             results[acc] = {'acc': acc,
-                                   'date': datetime.strptime(accdate + ' ' + acctime, '%m/%d/%Y %H:%M'),
+                            'accdate': datetime.strptime(accdate + ' ' + acctime, '%m/%d/%Y %H:%M'),
                           'name': name,
                           'mrn': mrn,
                           'bdate': datetime.strptime(bdate, '%m/%d/%Y'),
@@ -60,19 +75,33 @@ class Test(object):
                           }
         self.results = results
         return results
-    
+
     def make_identifier(self, acc):
-        id_args = [self.results[acc][i] for i in ['name', 'mrn', 'acc', 'accdate']]
+        id_args = [self.results[acc][i] for i in ['name', 'mrn', 'acc']]
+        id_args.append(self.results[acc]['accdate'].strftime('%Y.%m.%d.%H.%M'))
         return "%s %s %s %s" % tuple(id_args)
 
-    def replace(astring, adict, find=r"<(\w+)>"):
+    def make_report(self, acc, report_text, test_name, outdir, for_review=False):
+        nametext = self.make_identifier(acc)
+        if for_review:
+            f = open(path.join(outdir, '__review__%s.%s.txt' % (nametext, test_name)),'w')
+        else:
+            f = open(path.join(outdir, '%s.%s.txt' % (nametext, test_name)),'w')
+        f.write(report_text)
+        f.close()
+
+    def make_reports(self, report_text, test_name, outdir):
+        for result in self.results.values():
+            self.make_report(result['acc'], report_text, test_name, outdir)
+
+    def replace(self, astring, adict, find=r"<(\w+)>"):
       expr = re.compile(find, re.M)
       astring= str(astring)
       match = expr.search(astring)
       while match is not None:
         stringEnd = astring[match.end():]
         if match.group(1) in adict:
-          matchStr = replace(adict[match.group(1)], adict, find)
+          matchStr = self.replace(adict[match.group(1)], adict, find)
           astring = astring[:match.start()] + matchStr + stringEnd
         match = expr.search(astring, len(astring) - len(stringEnd))
       return astring    
@@ -100,16 +129,23 @@ class PT(Test):
                     
             for acc in accs:
                 # if the accession and the result are too far apart, there is a problem
+
                 if res_i < len(ress):
                     res = ress[res_i]
                 else:
+                    credit = re.search('REQUEST CREDITED', rslt[acc.start():acc.start()+700])
+                    if credit is not None:
+                        continue
                     raise ValueError("Accession %s beyond last result" % str(acc.groups()))
+                
                 print res.start() - acc.start()
                 print acc.start(), res.start(), acc.groups(), res.groups()
+                # if there is a credit between acc and res then go to the next acc
                 credit = re.search('REQUEST CREDITED', rslt[acc.start():res.start()])
-                    # if there is a credit then the next accession is the right one
+
                 if credit is not None:
-                        continue
+                    continue
+
                 if res.start() - acc.start() < 0:
                     raise ValueError("Could not parse result %s" % str(res.groups()))
                 acc, accdate, acctime = acc.groups()
@@ -118,7 +154,7 @@ class PT(Test):
                 results[accdatetime] = {'acc': acc,
                                         'name': name,
                                         'mrn': mrn,
-                                        'date': accdatetime,
+                                        'accdate': accdatetime,
                                         'val': Result(val, flag, {'ref': ref})
                                         }
                 # next iteration
@@ -141,79 +177,163 @@ class PT(Test):
         return latest_pts
 
 class HEXA(Test):
-    re_result = "{HEXA([\s\w]+)"
-    rvvt_result = {'POS': 'also positive', 'NEG': 'negative', 'EQUIV': 'equivocal'}
-    positive = "templates/HEXA Positive.txt"
-    negative = "templates/HEXA Negative.txt"
+    re_result = "{HEXA\s+(\w+)"
+    rvvt_result = {'POS': 'positive', 'NEG': 'negative', 'EQUIV': 'equivocal', 'BORDERNEG': 'borderline negative'}
+    positive = open("templates/HEXA Positive.txt").read()
+    negative = open("templates/HEXA Negative.txt").read()
+    equivocal = open("templates/HEXA Equivocal.txt").read()
 
-    def make_reports(self, rvvt_results, outdir='.'):
-        for interpreted_result in self.interpret(rvvt_results):
-            template = interpreted_result.pop('template')
-            text = super(HEXA, self).make_reports(template, interpreted_result)
-            outfile = interpreted_result['identifier'] + ' HEXA.txt'
-            f = open(path.join(outdir, outfile), 'wU')
-            f.write(text.replace('\n', '\r\n'))
-            f.close()
 
-    def interpret(self, rvvt_results):
+    def parse_result(self, result):
+        return HEXAresult(result[0])
+
+    def make_reports(self, rvvt, inr, outdir):
+        rvvt.interpret(inr)
+        for result, args in self.interpret():
+            # don't need to make a report if negative
+            if args['result'] == 'NEG':
+                continue
+            if result['acc'] in rvvt.results:
+                rvvtr = rvvt.interpretations[result['acc']]['result']
+                args['rvvt_result'] = self.rvvt_result[rvvtr]
+                if rvvtr == 'NEG' and args['result'] == 'NEG':
+                    args['both_neg'] = self.both_neg
+                else:
+                    args['both_neg'] = ''
+            else:
+                # TODO:  flag this report for review?
+                args['rvvt_result'] = '[positive/equivocal/negative]'
+                args['for_review'] = True
+            report_text = self.replace(args['template'], args)
+            if 'for_review' in args:
+                self.make_report(result['acc'], report_text, 'HEXA', outdir, for_review=True)
+            else:
+                self.make_report(result['acc'], report_text, 'HEXA', outdir)
+
+    def interpret(self):
+        self.interpretations = {}
         for acc in self.results:
             template_args = {}
             template_args['identifier'] = self.make_identifier(acc)
-            if acc in rvvt_results:
-                template_args['rvvt_result'] = hexa_rvvt_vals[rvvt_results[acc]['result']]
-            else:
-                # TODO:  check to see if the value is _always_ negative if not returned
-                template_args['rvvt_result'] = hexa_rvvt_vals['NEG']
             if self.results[acc]['result'].strip() == "POS":
+                template_args['result'] = 'POS'
                 template_args['template'] = self.positive
             elif self.results[acc]['result'].strip() == "EQHEX":
-                template_args['template'] = self.equivalent
+                template_args['result'] = 'EQUIV'
+                template_args['template'] = self.equivocal
+            elif self.results[acc]['result'].strip() == "NEG":
+                template_args['result'] = 'NEG'
+                template_args['template'] = self.negative
             else:
-                print "result not interpretable: %s\n%s" % (self.results[acc]['result'], i)
+                print "result not interpretable: %s\n%s" % (self.results[acc]['result'], self.results[acc])
                 import pdb; pdb.set_trace()
-            yield template_args
+            self.interpretations[acc] = template_args
+        return [(self.results[i], self.interpretations[i]) for i in self.results]
 
 class RVVT(Test):
     result_names = ["DVVTS", "DISS", "DVVN", "ISR", "DVVCS", "DVVCR"]
-    re_result = "\s+".join(["{%s([\s\d\.\,\*\w]+) \(RVVTD,6910\)" % name for name in result_names])
+    re_result = "\s+".join(["{%s([\s\d\.\,\*\w]+) \(RVVTD,\d+\)" % name for name in result_names])
 
-    hexa_result = {'POS': 'also positive', 'NEG': 'negative', 'EQUIV': 'equivocal'}
+    hexa_result = {'POS': 'positive', 'NEG': 'negative', 'EQUIV': 'equivocal'}
     strong_positive = "; in this sample a false positive is however unlikely given the strength of the positive result."
     both_neg = "Given the negative results in both the RVVT and Lupus Anticoagulant by HEXA assays, the findings are negative for Lupus Anticoagulant."
 
-    positive = "tempates/RVVT Positive.txt"
-    equivocal = "tempates/RVVT Equivocal.txt"
-    borderline_negative = "tempates/RVVT Borderline.txt"
-    negative = "tempates/RVVT Negative.txt"
+    positive = open("templates/RVVT Positive.txt").read()
+    equivocal = open("templates/RVVT Equivocal.txt").read()
+    borderline_negative = open("templates/RVVT Borderline.txt").read()
+    negative = open("templates/RVVT Negative.txt").read()
 
 
-    equiv_paragraph = {'MANUFACTURER':  """The RVVT test is positive for lupus anticoagulant according to the manufacturer’s guidelines for the interpretation of test results.   However, the RVVT test does not meet criteria for lupus anticoagulant according to published international guidelines (J Thromb Haemost 2009; 7: 1737-40).  The result is therefore EQUIVOCAL.""",
-                       'ISTH': """The RVVT test is negative for lupus anticoagulant according to the manufacturer’s guidelines for the interpretation of test results.   However, the results appear to be meet criteria for lupus anticoagulant according to published international guidelines (J Thromb Haemost 2009; 7: 1737-40).   The results are therefore EQUIVOCAL. """,
-                       'ISTH PT': """Although the RVVT test is positive for lupus anticoagulant according to the manufacturer’s guidelines for interpretation, review of the clinical history indicates [current warfarin therapy/a prolonged prothrombin time] and the results do not definitively demonstrate an inhibitor, a criterion for lupus anticoagulant according to published international guidelines (J Thromb Haemost 2009; 7: 1737-40).  Therefore, the result is EQUIVOCAL."""}
+    equiv_paragraph = {'MANUFACTURER':  """The RVVT test is positive for lupus anticoagulant according to the manufacturer's guidelines for the interpretation of test results.   However, the RVVT test does not meet criteria for lupus anticoagulant according to published international guidelines (J Thromb Haemost 2009; 7: 1737-40).  The result is therefore EQUIVOCAL.""",
+                       'ISTH': """The RVVT test is negative for lupus anticoagulant according to the manufacturer's guidelines for the interpretation of test results.   However, the results appear to be meet criteria for lupus anticoagulant according to published international guidelines (J Thromb Haemost 2009; 7: 1737-40).   The results are therefore EQUIVOCAL. """,
+                       'ISTH PT': """Although the RVVT test is positive for lupus anticoagulant according to the manufacturer's guidelines for interpretation, review of the clinical history indicates [current warfarin therapy/a prolonged prothrombin time] and the results do not definitively demonstrate an inhibitor, a criterion for lupus anticoagulant according to published international guidelines (J Thromb Haemost 2009; 7: 1737-40).  Therefore, the result is EQUIVOCAL."""}
  
     MANUF_THRESH = {'DVVTS': 43.2, 'DVVCR': 1.17}
-    ISTH_THRESH = {'DVVTS': 44.5, 'DVVCR': 1.07}
+    ISTH_THRESH = {'DVVTS': 44.5, 'DVVCR': 1.07, 'DISS': 41.6}
     DISS_ALT = 44.5
-    DISS_THRESH = 41.6
-    
+    ISR_ALT = 1.09
+
     def parse_result(self, result):
         rdict = {}
         for i in range(len(result)):
-            rdict[self.result_names[i]] = RVVTResult(result[i])
+            rdict[self.result_names[i]] = RVVTresult(result[i])
         return rdict
 
-    def requires_pt(self):
+    def check_thresh(self, result, thresh):
+        criteria_met = 0
+        for i in thresh:
+            if result['result'][i] > thresh[i]:
+                criteria_met += 1
+        if criteria_met == len(thresh):
+            return True
+        else:
+            return False
+
+    def interpret(self, inr):
+        self.interpretations = {}
+        latest_pts = inr.get_latest()
+        for acc in self.results:
+            template_args = {}
+            template_args['identifier'] = self.make_identifier(acc)
+            result = self.results[acc]
+            meets_manuf = self.check_thresh(result, self.MANUF_THRESH)
+            meets_isth = self.check_thresh(result, self.ISTH_THRESH)
+            
+            # if the result is borderline, see if the pt was elevated
+            if meets_isth and result['DISS'] < self.DISS_ALT:
+                if result['mrn'] in inr.results:
+                    if inr.results[result['mrn']]['result'] > 1.2:
+                        meets_isth = False
+
+            # determine whether pos, equiv, or neg
+            if meets_manuf and meets_isth:
+                template_args['result'] = 'POS'
+                template_args['template'] = self.positive
+            elif meets_manuf and not(meets_isth):
+                if result['result']['ISR'] > self.ISR_ALT:
+                    template_args['result'] = 'EQUIV'
+                    template_args['template'] = self.equivocal
+                    template_args['equiv_paragraph'] = self.equiv_paragraph['MANUFACTURER']
+                else:
+                    template_args['result'] = 'BORDERNEG'
+                    template_args['template'] = self.borderline_negative
+            elif not(meets_manuf) and meets_isth:
+                template_args['result'] = 'EQUIV'
+                template_args['template'] = self.equivocal
+                template_args['equiv_paragraph'] = self.equiv_paragraph['ISTH']            
+            else:
+                    template_args['result'] = 'NEG'
+                    template_args['template'] = self.negative
+                
+            # print "result not interpretable: %s\n%s" % (self.results[acc]['result'], i)
+            #import pdb; pdb.set_trace()
+            self.interpretations[acc] = template_args
+        return [(self.results[i], self.interpretations[i]) for i in self.results]
+
+    def requires_inr(self):
         pt_mrns = []
         for test in self.results.values():
-            if test['result']['DVVCR'] > self.ISTH_THRESH['DVVCR']:
-                if self.DISS_THRESH < test['result']['DISS'] <= self.DISS_ALT:
-                    pt_mrns.append(test['mrn'])
+            # if test['result']['DVVCR'] > self.ISTH_THRESH['DVVCR']:
+            #     if self.DISS_THRESH < test['result']['DISS'] <= self.DISS_ALT:
+            pt_mrns.append(test['mrn'])
         return pt_mrns
-
-    def interpret(self, latest_pts):
-        for test in self.results.values():
     
-        # if PTT prolonged
-        pass
-        # PL dependent
-    
+    def make_reports(self, hexa, inr, outdir):
+        hexa.interpret()
+        for result, args in self.interpret(inr):
+            if result['acc'] in hexa.results:
+                hexar = hexa.interpretations[result['acc']]['result']
+            else:
+                # TODO: fix this to download completed results as well
+                # assume that the test was negative if not included
+                hexar = 'NEG'
+            args['hexa_result'] = self.hexa_result[hexar]
+            if hexar == 'NEG' and args['result'] == 'NEG':
+                args['both_neg'] = self.both_neg
+            else:
+                args['both_neg'] = ''
+            report_text = self.replace(args['template'], args)
+            if 'for_review' in args:
+                self.make_report(result['acc'], report_text, 'HEXA', outdir, for_review=True)
+            else:
+                self.make_report(result['acc'], report_text, 'HEXA', outdir)
